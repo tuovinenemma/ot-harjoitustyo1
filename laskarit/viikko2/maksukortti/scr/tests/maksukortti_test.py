c7d237c28f8f36595cb3c7a8764050a3f155d61d
Emma Tuovinen <emmasofia@live.fi>
2022-03-28 20:47:26 +0300

    maksukortti testi muokkaus

--- laskarit/viikko2/maksukortti/scr/tests/maksukortti_test.py
+++ laskarit/viikko2/maksukortti/scr/tests/maksukortti_test.py
@@ -5,7 +5,11 @@ class TestMaksukortti(unittest.TestCase):
     def setUp(self):
         print("Set up goes here")
 
-    def test_konstruktori_asettaa_saldon_oikein(self):
-	kortti = Maksukortti(10)
-	self.assertEqual(str(kortti), "Kortilla on rahaa 9 euroa")
+    def test_syo_edullisesti_vahentaa_saldoa_oikein(self):
+    kortti = Maksukortti(10)
+
+    kortti.syo_edullisesti()
+
+    self.assertEqual(str(kortti), "Kortilla on rahaa 7.5 euroa")
+
 
