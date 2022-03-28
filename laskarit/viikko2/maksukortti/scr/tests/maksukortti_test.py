738c7ad116d284f402ffba09cfcd28527e3dda88
Emma Tuovinen <emmasofia@live.fi>
2022-03-28 20:59:46 +0300

    scr

--- laskarit/viikko2/maksukortti/scr/tests/maksukortti_test.py
+++ laskarit/viikko2/maksukortti/scr/tests/maksukortti_test.py
@@ -4,12 +4,23 @@ from maksukortti import Maksukortti
 
 class TestMaksukortti(unittest.TestCase):
     def setUp(self):
-        print("Set up goes here")
+        self.kortti = Maksukortti(10)
 
     def test_konstruktori_asettaa_saldon_oikein(self):
-        kortti = Maksukortti(10)
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 10 euroa")
 
-        vastaus = str(kortti)
+    def test_syo_edullisesti_vahentaa_saldoa_oikein(self):
+        self.kortti.syo_edullisesti()
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 7.5 euroa")
 
-        self.assertEqual(vastaus, "Kortilla on rahaa 10 euroa")
+    def test_syo_maukkaasti_vahentaa_saldoa_oikein(self):
+        self.kortti.syo_maukkaasti()
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 6 euroa")
+
+
+    def test_syo_edullisesti_ei_vie_saldoa_negatiiviseksi(self):
+        self.kortti.syo_maukkaasti()
+        self.kortti.syo_maukkaasti()
+        self.kortti.syo_edullisesti()
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 2 euroa")
 
