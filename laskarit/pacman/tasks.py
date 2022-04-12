656221df208146290b228c6df6b31d5d0809ae33
Emma Tuovinen <emmasofia@live.fi>
2022-04-12 18:56:58 +0300

    muutos

---
+++ laskarit/pacman/tasks.py
@@ -0,0 +1,18 @@
+from invoke import task
+
+
+@task
+def start(ctx):
+    ctx.run("python src/peli.py", pty=True)
+    
+@task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
+    
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest", pty=True)
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html", pty=True)
